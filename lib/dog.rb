3f1e970c069b276c9e43a9449420a3125b4959a1
Luis <lvas248@gmail.com>
2022-09-26 15:29:28 -0400

    commit tree 56a5c62da4ca6b8988095683a1a03c79f821aeeb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+
+
+sasha = Dog.new
+
+sasha.name = "Sasha"
+
+sasha.name
